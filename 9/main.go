32a0b3cd5fcd95cbb993d419e7cedcc9bb897380
Alim <TrakTorrrR12@yandex.ru>
2025-08-17 15:15:53 +0300

    Решение 9

--- 9/main.go
+++ 9/main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+func processNumber(ctx context.Context, arr []int, numbersCh chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(numbersCh)
+	for _, v := range arr {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Завершение обработки чисел")
+			return
+		case numbersCh <- v:
+		}
+	}
+}
+
+func doubleNumber(numbersCh, doubleNumbersCh chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for number := range numbersCh {
+
+		doubleNumbersCh <- number * 2
+	}
+	close(doubleNumbersCh)
+
+}
+
+func printNumbers(doubleNumbersCh chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for number := range doubleNumbersCh {
+		fmt.Println(number)
+	}
+}
+
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	numbersCh := make(chan int, 10)
+	doubleNumbersCh := make(chan int, 10)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT)
+	defer cancel()
+
+	arr := []int{10, 20, 30, 45, 2100}
+
+	go processNumber(ctx, arr, numbersCh, &wg)
+	go doubleNumber(numbersCh, doubleNumbersCh, &wg)
+	go printNumbers(doubleNumbersCh, &wg)
+	wg.Wait()
+	fmt.Println("Завершение основной программы")
 
 }
